10903dbc1e0b1bc64c379990487d26fe90228f86
abedin_raju <abedin.sm@gmail.com>
2021-03-13 12:46:07 +0600

    student database form design

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Development Area</title>
+	<!-- ALL CSS FILES  -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+<body>
+	
+	
+
+	<div class="wrap shadow">
+		<div class="card">
+			<div class="card-body">
+				<h2>Add New Students</h2>
+				<form action="">
+					<div class="form-group">
+						<label for="">Name</label>
+						<input class="form-control" type="text">
+					</div>
+					<div class="form-group">
+						<label for="">Email</label>
+						<input class="form-control" type="text">
+					</div>
+					<div class="form-group">
+						<label for="">Cell</label>
+						<input class="form-control" type="text">
+					</div>
+					<div class="form-group">
+						<label for="">Username</label>
+						<input class="form-control" type="text">
+					</div>
+
+                    <div class="form-group">
+						<label for="">Gender</label> <br>
+						<input name="gender" class="" type="radio" id="male"> <label for="male">Male</label>
+						<input name="gender" class="" type="radio" id="female"> <label for="female">Female</label>
+					</div>
+
+					<div class="form-group">
+					<label for="">Shift</label>
+
+					<select class="form-control" name="" id="">
+					<option value="">-Select-</option>
+					<option value="">Day</option>
+					<option value="">Evening</option>
+					</select>
+					
+					
+					</div>
+
+					<div class="form-group">
+						<label for="">Age</label>
+						<input class="form-control" type="text">
+					</div>
+
+					<div class="form-group">
+						<label for="">Location</label>
+						<select class="form-control" name="" id="">
+						<option value="">-Select-</option>
+						<option value="">Dhaka</option>
+						<option value="">Chittagong</option>
+						<option value="">Rajshahi</option>
+						<option value="">Khulna</option>
+						<option value="">Barishal</option>
+						<option value="">Sylhet</option>
+						<option value="">Rongpur</option>
+						<option value="">Mymensingh</option>
+						
+						
+						</select>
+						
+					</div>
+
+					<div class="form-group">
+						<label for="">Photo</label>
+						<input class="form-control-file" type="file">
+					</div>
+
+					<div class="form-group">
+						<input class="btn btn-primary" type="submit" value="Add Students">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+     <br>
+     <br>
+     <br>
+     <br>
+     <br>
+     <br>
+
+
+
+
+
+	<!-- JS FILES  -->
+	<script src="assets/js/jquery-3.4.1.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/custom.js"></script>
+</body>
+</html>
